383ffd821456d7a7a90f7341172bc7e3fc142d69
Santiago Zavatti <134091360+ZavSan95@users.noreply.github.com>
2024-09-12 13:02:40 -0300

    Update step1.php

--- web/views/pages/comercios/formularios/renovacion/steps/step1.php
+++ web/views/pages/comercios/formularios/renovacion/steps/step1.php
@@ -1,34 +1,51 @@
 <div style="margin-bottom:16px;">
-        <label for="rubro">Seleccione Rubro*</label>
-        <select id="rubro" name="rubro" class="form-control required">
-            <option value="0" disabled selected>Seleccione Rubro*</option>
-            <option value="1">Rubro 1</option>
-        </select>
-    </div>
+    <label for="rubro">Seleccione Rubro*</label>
+    <select id="rubro" name="rubro" class="form-control required">
+        <option value="0" disabled selected>Seleccione Rubro*</option>
+        <option value="1">Rubro 1</option>
+    </select>
+</div>
 
-    <div style="margin-bottom:16px;">
-        <label for="cuenta_drei">Nº CUENTA DREI*</label>
-        <input id="cuenta_drei" type="number" name="cuenta_drei" class="form-control required"
-            placeholder="Nº CUENTA DREI*" required>
-    </div>
+<div style="margin-bottom:16px;">
+    <label for="cuenta_drei">Numero Cuenta DREI*</label>
+    <input id="cuenta_drei" type="number" name="cuenta_drei" class="form-control required" placeholder="Numero Cuenta DREI*"
+        required>
+</div>
 
-    <div style="margin-bottom:16px;">
-        <label for="fecha_inicio">Fecha Inicio de la Actividad</label>
-        <input id="fecha_inicio" type="date" name="fecha_inicio" class="form-control required"
-            placeholder="Fecha Inicio de la Actividad" required>
-    </div>
+<div style="margin-bottom:16px;">
+    <label for="fecha_inicio">Fecha Inicio de la Actividad</label>
+    <input id="fecha_inicio" type="date" name="fecha_inicio" class="form-control required"
+        placeholder="Fecha Inicio de la Actividad" required>
+</div>
 
-    <div style="margin-bottom:16px;">
-        <label for="nombre_fantasia">Nombre de Fantasía*</label>
-        <input id="nombre_fantasia" type="text" name="nombre_fantasia" class="form-control required"
-            placeholder="Nombre de Fantasía*" required>
-    </div>
+<div style="margin-bottom:16px;">
+    <label for="nombre_fantasia">Nombre de Fantasía*</label>
+    <input id="nombre_fantasia" type="text" name="nombre_fantasia" class="form-control required"
+        placeholder="Nombre de Fantasía*" required>
+</div>
 
-    <div style="margin-bottom:16px;">
-        <label for="tipo_contribuyente">Tipo de Contribuyente*</label>
-        <select id="tipo_contribuyente" name="tipo_contribuyente" class="form-control required">
-            <option value="0" disabled selected>Elija una opción*</option>
-            <option value="1">Persona Física</option>
-            <option value="2">Persona Jurídica</option>
-        </select>
-    </div>
+<div style="margin-bottom:16px;">
+    <label for="tipo_contribuyente">Tipo de Contribuyente*</label>
+    <select id="tipo_contribuyente" name="tipo_contribuyente" class="form-control required">
+        <option value="0" disabled selected>Elija una opción*</option>
+        <option value="1">Persona Física</option>
+        <option value="2">Persona Jurídica</option>
+    </select>
+</div>
+
+<h4>DATOS DEL LOCAL</h4>
+
+<div style="margin-bottom:16px;">
+    <label for="direccion">Dirección</label>
+    <input id="direccion" type="all" name="direccion" class="form-control required" placeholder="Ingrese dirección" required>
+</div>
+
+<div style="margin-bottom:16px;">
+    <label for="piso">Piso</label>
+    <input id="piso" type="all" name="piso" class="form-control" placeholder="Piso" >
+</div>
+
+<div style="margin-bottom:16px;">
+    <label for="departamento">Departamento/Oficina/Local</label>
+    <input id="departamento" type="all" name="departamento" class="form-control" placeholder="Número Departamento/Oficina/Local" >
+</div
